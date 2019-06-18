67954329326b840e5e613139e728e28a075c6113
Ian Axelrod <ian@themuse.com>
2019-06-18 18:08:09 -0400

    Add list of permissions used by execution_role_arn
    PLAT-2040

--- modules/lambda-function/variables.tf
+++ modules/lambda-function/variables.tf
@@ -4,6 +4,6 @@ variable "name" {
 }
 
 variable "execution_role_arn" {
-  description = "IAM role arn to use for lambda execution. If not supplied, this module will create a role with necessary permissions."
+  description = "IAM role arn to use for lambda execution. If not supplied, this module will create a role with necessary permissions. These permissions are 'cloudwatch:PutMetricData', 'ecs:DescribeServices', 'sqs:GetQueueUrl', 'sqs:GetQueueAttributes', 'logs:CreateLogGroup', 'logs:CreateLogStream', and 'logs:PutLogEvents'."
   default     = ""
 }
